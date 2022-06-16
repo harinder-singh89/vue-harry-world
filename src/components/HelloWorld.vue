422b6d6f8cc5e92229dd2e7989e76ae542865ed5
Harinder Singh <harinder@ranadev.io>
2022-06-16 17:50:19 +0530

    initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="hello">
+    Hello World
+    <p>{{title}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props:{
+    title: {
+      default: "title test"
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
---
+++ src/install.js
@@ -0,0 +1,19 @@
+import HelloWorld from "./components/HelloWorld.vue";
+
+const HelloWorldSimple = {
+  install(Vue, options) {
+    // Let's register our component globally
+    // https://vuejs.org/v2/guide/components-registration.html
+    Vue.component("hello-world", {
+      extends: HelloWorld,
+      props: options,
+    });
+  },
+};
+
+// Automatic installation if Vue has been added to the global scope.
+if (typeof window !== "undefined" && window.Vue) {
+  window.Vue.use(HelloWorldSimple);
+}
+
+export default HelloWorldSimple;
